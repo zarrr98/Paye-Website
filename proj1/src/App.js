0dccba778f11e7ebea92ccd547f41bda29408aa4
zahra <sabah.z3939@gmail.com>
2020-09-23 16:26:27 +0330

    add a simple, design-free alert page

--- proj1/src/App.js
+++ proj1/src/App.js
@@ -3,15 +3,13 @@ import logo from "./logo.svg";
 import "./assets/css/style.css";
 import { FetchData, PutData } from "./utils/services";
 import { URL } from "./utils/configs";
-import SignupPage from './js/screens/SignupPage'
-import LoginPage from './js/screens/LoginPage';
-import {BrowserRouter as Router , Route , Switch} from 'react-router-dom'
+import SignupPage from "./js/screens/SignupPage";
+import LoginPage from "./js/screens/LoginPage";
+import AlertPage from "./js/screens/AlertPage";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends React.Component {
-  state = {
-   
-  };
-
+  state = {};
 
   render() {
     return (
@@ -21,14 +19,16 @@ class App extends React.Component {
             <h2>main page or sth </h2>
           </Route>
           <Route path={"/signup"}>
-            <SignupPage/>
+            <SignupPage />
           </Route>
           <Route path={"/login"}>
-            <LoginPage/>
+            <LoginPage />
+          </Route>
+          <Route path={"/alert"}>
+            <AlertPage/>
           </Route>
         </Switch>
       </Router>
-      
     );
   }
 }
